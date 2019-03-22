73e3027127bb1fb288d505596a4a7c81eced77e5
Valerie Claessen <valerie.claessen@hhu.de>
2019-03-22 11:19:39 +0100

    labeled_data beendet und supreme_conversation und twitter_customer fertig und begonnen

--- Flask/static/index_script.js
+++ Flask/static/index_script.js
@@ -4,3 +4,8 @@
 function myFunction() {
     document.getElementById("demo").style.color = "red";
 }
+
+function oeffnefenster (url) {
+   fenster = window.open(url, "fenster1", "width=600,height=400,status=yes,scrollbars=yes,resizable=yes");
+   fenster.focus();
+}
--- Flask/test_flask.py
+++ Flask/test_flask.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_socketio import SocketIO
 
+from Flask.test_save_chat import write_to_file
+from flask_bootstrap import Bootstrap
+
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
 @app.route('/')
 def sessions():
-    return render_template('session.html')
+    return render_template('index.html')
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
@@ -15,6 +20,9 @@ def messageReceived(methods=['GET', 'POST']):
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
+    if (json.get("user_name") != "None"):
+        write_to_file(json.get("message"),json.get("user_name"))
+        #print(json.get("user_name"))
     socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
