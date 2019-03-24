f0eedb199c55aec4890ca321b08bcea89653ea68
Sabrina Wirkner <sabrina.wirkner@hhu.de>
2019-03-24 15:52:36 +0100

    chatroom with multiple usages

--- Flask/test_flask.py
+++ Flask/test_flask.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO
 
-from Flask.test_save_chat import write_to_file
+from test_save_chat import write_to_file
 from flask_bootstrap import Bootstrap
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def sessions():
     return render_template('index.html')
 
@@ -22,7 +21,7 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     if (json.get("user_name") != "None"):
         write_to_file(json.get("message"),json.get("user_name"))
-        #print(json.get("user_name"))
+        print(json.get("user_name"))
     socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
