d784057fe4b7c74dfb7af2f2377d26354df081ac
Sabrina Wirkner <sabrina.wirkner@hhu.de>
2019-03-24 18:46:56 +0100

    chat with cards and scrollbar

--- Flask/static/index_script.js
+++ Flask/static/index_script.js
@@ -21,8 +21,11 @@ var form = $( 'form' ).on( 'submit', function( e ) {
 socket.on( 'my response', function( msg ) {
 console.log( msg )
 if( typeof msg.user_name !== 'undefined' ) {
-  $( 'h3' ).remove()
-  $( 'div.message_holder' ).append( '<div><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>' )
+  /*$( 'h3' ).remove()'/
+  /*$( 'div.message_holder' ).append( '<div><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>' )*/
+  /*var currentVal = $('#chatbox').append('<b>' + msg.user_name + '</b> ' + msg.message);*/
+    $('div.message_holder').append('<div class="card"><div class="card-body"><h6 class="card-subtitle mb-2 text-muted text-left">' + msg.user_name + '</h6><p class="card-text float-${side}">' + msg.message + '</p></div></div>');
+    updateScroll();
 }
 })
 
@@ -33,4 +36,9 @@ function myFunction() {
 function oeffnefenster (url) {
    fenster = window.open(url, "fenster1", "width=600,height=400,status=yes,scrollbars=yes,resizable=yes");
    fenster.focus();
+}
+
+function updateScroll(){
+    var element = document.getElementById("message_holder");
+    element.scrollTop = element.scrollHeight;
 }
