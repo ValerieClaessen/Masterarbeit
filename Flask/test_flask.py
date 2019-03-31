02ad7b12dd331f964c82bac3f9d2dac0d4f6831c
Valerie Claessen <valerie.claessen@hhu.de>
2019-03-31 19:44:31 +0200

    labeled_data beendet und supreme_conversation und twitter_customer fertig und begonnen

--- Flask/test_flask.py
+++ Flask/test_flask.py
@@ -3,9 +3,8 @@ from flask_socketio import SocketIO
 
 from flask_bootstrap import Bootstrap
 
-from test_save_chat import write_to_file
-
-import machine_learning
+from Flask import machine_learning
+from Flask.test_save_chat import write_to_file
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -23,18 +22,23 @@ def messageReceived(methods=['GET', 'POST']):
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     currentSocketId = request.sid   #gibt die individuelle ID des aktuellen User an
-    print(currentSocketId)
+    print('current socketID: ' + currentSocketId)
     print('received my event: ' + str(json))
-    if (json.get("user_name") is not None):
-        write_to_file(json.get("message"),json.get("user_name"), currentSocketId)
+
 
     if (json.get("message") is not None):
         class_cb = machine_learning.use_svm(json.get("message"))
-        print(json.get("message"))
         print("Cyberbullying: ", class_cb[0])
         print("Hate Speech: ", class_cb[1])
         json["cb"] = class_cb[0]
         json["hs"] = class_cb[1]
+
+    if (json.get("user_name") is not None):
+        class_cb = machine_learning.use_svm(json.get("message"))
+        json["cb"] = class_cb[0]
+        json["hs"] = class_cb[1]
+        print(json.get("message"))
+        write_to_file(json.get("message"),json.get("user_name"), currentSocketId, str(json["cb"]), str(json["hs"]), json.get("evaluation"))
 
     socketio.emit('my response', json, callback=messageReceived)
 
--- Flask/test_save_chat.py
+++ Flask/test_save_chat.py
@@ -1,8 +1,18 @@
+import datetime
 
-def write_to_file(text, username, userID):
 
-    f = open('file.txt', 'a')
-    f.write("user = " + str(username) + " chattext = " + str(text) + " ID = " + str(userID) + '\n')
+def write_to_file(text, username, userID, comp_cb, comp_hs, eval):
+
+    x = datetime.datetime.now()
+    date = str(x.date())
+    print(date)
+
+    if date == "2019-03-31":
+        f = open('file_2019-03-31.txt', 'a')
+        f.write("user = " + str(username) + " chattext = " + str(text) + " ID = " + str(userID) + " computed CB = " + str(comp_cb) + " computed HS = " + str(comp_hs) + " evaluation = " + str(eval) + '\n')
+    else:
+        f = open('file.txt', 'a')
+        f.write("user = " + str(username) + " chattext = " + str(text) + " ID = " + str(userID) + '\n')
 
 
 #write_to_file("test", 23)
